07bc9924e8e967763fd83e03465d8c60ade5d591
gordonyaogz <gordonyaogz@gmail.com>
2022-03-17 23:24:13 +0800

    加密用户密码存入表中，增加用户。

--- backend/api/v1/endpoints/login.py
+++ backend/api/v1/endpoints/login.py
@@ -1,7 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from backend.core import verify_password
+from backend.core.security import create_access_token
+from backend.models import User
+from backend.schemas import UserIn_Pydantic
 
 login = APIRouter(tags=["认证相关"])
 
 @login.post("/login", summary="登录")
-async def user_login():
-    pass
+async def user_login(form_data: OAuth2PasswordRequestForm = Depends()):
+    if user := await User.get(username=form_data.username):
+        if verify_password(form_data.password,user.password):
+            return {"access_token":create_access_token({"id":user.pk}),"token_type":"bearer"}
+        return {"msg":"账号或密码错误"}
+
+@login.post("/user", summary="用户新增")
+async def user_create(user: UserIn_Pydantic):
+    await User.create(**user.dict())
+    return 1
